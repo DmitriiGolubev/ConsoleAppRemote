d61c80d7876c8d4356f5563942b040673ac9e5c1
golubevdi <golubevdi@DESKTOP-J2854HB>
2026-07-04 11:01:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person pers = new Person(18);
+            if (pers.IsAdult())
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+
+            Person.IncreaseLegalAge(1);
+            if (pers.IsAdult())
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+
+    class Person
+    {
+        
+        public Person(int age)
+        {
+            Age = age;
+        }
+
+        public int Age { get; set; }
+        int LegalAge = 18;
+
+
+        public bool IsAdult()
+        {
+            if (Age == LegalAge) 
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+
+        public static void IncreaseLegalAge(int n)
+        {
+            Age += n;
+        }
+    }
+}
