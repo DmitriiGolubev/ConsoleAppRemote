b56a2392351348690bdd15c1b09bdb300d7461de
golubevdi <golubevdi@DESKTOP-J2854HB>
2026-07-09 16:21:31 +0300

    Решил подзадачу 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using System.Diagnostics;
-using System.Drawing;
-using System.Reflection;
-using System.Text;
-
+﻿using System;
 
 namespace ConsoleApp
 {
@@ -10,47 +6,8 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            Person pers = new Person(18);
-            if (pers.IsAdult())
-                Console.WriteLine("YES");
-            else
-                Console.WriteLine("NO");
-
-            Person.IncreaseLegalAge(1);
-            if (pers.IsAdult())
-                Console.WriteLine("YES");
-            else
-                Console.WriteLine("NO");
-        }
-    }
-
-    class Person
-    {
-        
-        public Person(int age)
-        {
-            Age = age;
-        }
-
-        public int Age { get; set; }
-        int LegalAge = 18;
-
-
-        public bool IsAdult()
-        {
-            if (Age == LegalAge) 
-            {
-                return true;
-            }
-            else 
-            { 
-                return false; 
-            }
-        }
-
-        public static void IncreaseLegalAge(int n)
-        {
-            Age += n;
+            // Это изменение для подзадачи 1
+            Console.WriteLine("Hello World!");
         }
     }
 }
